f7260039f85d314b030c74a9af3db2f4bb298170
Gabriela Araujo <110007206+Gabyara237@users.noreply.github.com>
2025-01-10 15:34:01 -0800

    Implement GET All Todos

--- src/main/java/com/teamtreehouse/techdegrees/App.java
+++ src/main/java/com/teamtreehouse/techdegrees/App.java
@@ -1,15 +1,36 @@
 package com.teamtreehouse.techdegrees;
 
 
-import static spark.Spark.get;
-import static spark.Spark.staticFileLocation;
+import com.google.gson.Gson;
+import com.teamtreehouse.techdegrees.dao.Sql2oToDoDao;
+import com.teamtreehouse.techdegrees.dao.TodoDao;
+import org.sql2o.Sql2o;
+
+import static spark.Spark.*;
 
 public class App {
 
     public static void main(String[] args) {
         staticFileLocation("/public");
 
-        get("/blah", (req, res) -> "Hello!");
+        String datasource = "jdbc:h2:~/tododb.db";
+        if(args.length > 0){
+            if (args.length !=2){
+                System.out.println("java Api <port> <datasource>");
+                System.exit(0);
+            }
+            port(Integer.parseInt(args[0]));
+            datasource = args[1];
+        }
+
+        Sql2o sql2o = new Sql2o(
+                String.format("%s;INIT=RUNSCRIPT from 'classpath:db/init.sql'", datasource)
+                ,"","");
+        TodoDao todoDao =  new Sql2oToDoDao(sql2o);
+        Gson gson = new Gson();
+
+        get("/api/v1/todos", "application/json", (req, resp) -> todoDao.findAll(),gson::toJson);
+
 
     }
 
